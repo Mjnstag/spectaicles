01690e6fb99f87023576c2f5d411927b9f9888d0
dandevri <hi@dandevri.es>
2023-03-16 11:52:52 +0100

    Add templating and routes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+/* Require libraries */
 const express = require('express')
 const app = express()
 const port = 3000
 
+/* Serve static files */
+app.use(express.static('static'))
+
+/* Set the templating engine */
+app.set('view engine', 'ejs')
+
+/* Render the routes */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('./pages/index')
 })
 
+app.get('/connect', (req, res) => {
+  res.render('./pages/connect')
+})
+
+/* Start the server */
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
