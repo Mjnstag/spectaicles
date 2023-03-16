e66f6e43e7086dc5071042064fc9200c96e19aee
Mjnstag <54844575+Mjnstag@users.noreply.github.com>
2023-03-16 13:58:29 +0100

    a

--- python/distancev2.py
+++ python/distancev2.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import imutils
 import cv2
@@ -16,8 +15,10 @@ def find_stickynote(image):
     cnts = imutils.grab_contours(cnts)
     c = max(cnts, key=cv2.contourArea)
     M = cv2.moments(c)
+
+    cv2.imshow("Gray", edged)
     # compute the bounding box of the of the note region and return it
-    return cv2.minAreaRect(c), [int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])]
+    return (cv2.minAreaRect(c))
 
 
 def distance_to_camera(known_width, focal_length, per_width):
@@ -28,28 +29,29 @@ def distance_to_camera(known_width, focal_length, per_width):
 KNOWN_DISTANCE = 20
 KNOWN_WIDTH = 16  # cm
 
-# load the reference image and find the reference object in the image
-ref_image = cv2.imread(r"img/test.jfif")
-ref_obj, ref_center = find_stickynote(ref_image)
-ref_width = ref_obj[1][0]
-
-# compute the focal length using the known distance and width of the reference object
-focal_length = (ref_width * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 # initialize the video capture object using the default camera
-cap = cv2.VideoCapture(1)
-
+cap = cv2.VideoCapture(0)
+rescale = False
 # loop over frames from the video stream
 while True:
     # read a frame from the video stream
     ret, frame = cap.read()
 
+    if rescale:
+        scale_percent = 60  # percent of original size
+        width = int(frame.shape[1] * scale_percent / 100)
+        height = int(frame.shape[0] * scale_percent / 100)
+        dim = (width, height)
+
+        frame = cv2.resize(frame, dim)
+
     # find the sticky note and its center in the frame
-    sticky_note, centre = find_stickynote(frame)
+    sticky_note = find_stickynote(frame)
 
     # compute the distance to the object using the calibrated camera parameters
     # dist = distance_to_camera(KNOWN_WIDTH, focal_length, sticky_note[1][0])
-    dist = distance_to_camera(6.3, 600, sticky_note[1][0])
+    dist = distance_to_camera(KNOWN_WIDTH, 600, sticky_note[1][0])
 
     # draw a bounding box and display the distance on the frame
     box = cv2.cv.BoxPoints(sticky_note) if imutils.is_cv2(
@@ -57,18 +59,16 @@ while True:
     box = np.int0(box)
     w, h = sticky_note[1]
 
-    # cv2.putText(frame, "{:.2f}cm".format(dist), (int(centre[0]), int(centre[1])),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 2)
     ratio = float(w)/h
     if ratio >= 0.8 and ratio <= 1.2 and w > 20 and h > 20:
         cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
-        cv2.putText(frame, "Distance: " + str(round((6.3*600/dist), ndigits=2)) + " cm",
+        cv2.putText(frame, "Distance: " + str(round((KNOWN_WIDTH*600/dist), ndigits=2)) + " cm",
                     (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 
     # show the frame
     cv2.imshow("Frame", frame)
 
-    key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1)
     if key == ord("q"):
         break
 
--- python/website/app.py
+++ python/website/app.py
@@ -3,13 +3,21 @@ from flask import Flask, render_template
 # Create the application.
 app = Flask(__name__)
 
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 # A simple page that says hello
 @app.route("/")
 def hello():
     return """Hello World!
-    <br><a href="/webcam">Webcam</a>"""
+    <br><a href="/webcam">Webcam</a>
+    <br><a href="/face_rec">face_rec</a>"""
 
 
 @app.route("/webcam")
 def webcam():
-    return render_template("webcam.html")
+    return render_template("webcam_py.html")
+
+
+@app.route("/face_rec")
+def face_rec():
+    return render_template("face_rec.html")
